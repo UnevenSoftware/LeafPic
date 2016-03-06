11ab69e2c68e35c39c268c802fcce06ea35f8a27
Gilbert Ndresaj <jibo95@gmail.com>
2016-03-06 14:26:14 +0000

    Synch

--- app/src/main/java/com/leafpic/app/AlbumsActivity.java
+++ app/src/main/java/com/leafpic/app/AlbumsActivity.java
@@ -550,10 +550,13 @@ public class AlbumsActivity extends ThemedActivity /*implements FolderChooserDia
                 } else {
                     Album album = albums.getAlbum(a.getTag().toString());
                     Intent intent = new Intent(AlbumsActivity.this, PhotosActivity.class);
-
+                    //
                     //intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     //intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    //
 
+
+                    /****/
                     Bundle b = new Bundle();
                     b.putParcelable("album", album);
                     intent.putExtras(b);
