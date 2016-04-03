061e229e8eba1c040809858850d7d2499b225827
DNLDsht <barbanish@gmail.com>
2016-04-03 16:06:43 +0200

    tweaks auto column number

--- app/src/main/java/com/leafpic/app/utils/Costants.java
+++ app/src/main/java/com/leafpic/app/utils/Costants.java
@@ -4,6 +4,6 @@ package com.leafpic.app.utils;
  * Created by dnld on 30/03/16.
  */
 public class Costants {
-    public static int ALBUM_CARD_WIDTH = 370;
-    public static int PHOTO_CARD_WIDTH = 280;
+    public static int ALBUM_CARD_WIDTH = 180;
+    public static int PHOTO_CARD_WIDTH = 120;
 }
--- app/src/main/java/com/leafpic/app/utils/Measure.java
+++ app/src/main/java/com/leafpic/app/utils/Measure.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
 
@@ -16,8 +17,7 @@ public class Measure {
 
     public static int pxToDp(int dp, Context c) {
         DisplayMetrics displayMetrics = c.getResources().getDisplayMetrics();
-        int px = Math.round(dp * (displayMetrics.ydpi / DisplayMetrics.DENSITY_DEFAULT));
-        return px;
+        return Math.round(dp * (displayMetrics.ydpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     public static int getScreenWidth(Context c){
@@ -29,14 +29,17 @@ public class Measure {
     }
 
     public static int getAlbumsColums(Context c){
-        int width = getScreenWidth(c);
-
-        return Math.round(width/Costants.ALBUM_CARD_WIDTH);
+        int n = Math.round(getDensity(c) / Costants.ALBUM_CARD_WIDTH);
+        return n < 2 ? 2 : n;
     }
 
     public static int getPhotosColums(Context c){
-        int width = getScreenWidth(c);
-        return Math.round(width/Costants.PHOTO_CARD_WIDTH);
+        int n = Math.round(getDensity(c)/ Costants.PHOTO_CARD_WIDTH);;
+        return n < 3 ? 3 : n;
+    }
+
+    public static int getDensity(Context c){
+        return Math.round((getScreenWidth(c) / c.getResources().getDisplayMetrics().density));
     }
 
     public static int getStatusBarHeight(Resources r) {
