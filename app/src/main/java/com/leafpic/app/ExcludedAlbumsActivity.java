3e31b89a889bafced582d055801edac17f1044d7
Gilbert Ndresaj <jibo95@gmail.com>
2016-04-04 20:55:41 +0100

    Synch

--- app/src/main/java/com/leafpic/app/ExcludedAlbumsActivity.java
+++ app/src/main/java/com/leafpic/app/ExcludedAlbumsActivity.java
@@ -29,7 +29,7 @@ public class ExcludedAlbumsActivity extends ThemedActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excluded);
-
+ 
 
 
     }
