3071e2bb9ed7cbd9d0400a58b04d24017f782f73
DNLDsht <dnld.sht@gmail.com>
2016-04-10 23:34:20 +0200

    TONS OF CHANGES & improvements merged albums act with photos act random tweaks

--- app/src/main/java/com/leafpic/app/CustomDialog.java
+++ app/src/main/java/com/leafpic/app/CustomDialog.java
@@ -7,7 +7,8 @@ import com.leafpic.app.Views.ThemedActivity;
  */
 public class CustomDialog extends ThemedActivity{
      //TODO: E TROPPO SBATTI FALLA TE STA CLASSE O ALMENO INIZIALA LOL
-    // ASSEGNATO A DONY <--IRREMOVIBILE
+    // ASSEGNATO A DONY <-- IRREMOVIBILE
     // PER LE DIALOG OCN LSOLO TESTO TIPO ELIMINA ECC USA TEXT DIALOG
-
+    // Mi rifuto solo per come l'hai inizata mi fai una classe dialog che eredita
+    // la classe activity bello RIPIGLIATI!!!!!
 }
--- app/src/main/java/com/leafpic/app/SplashScreen.java
+++ app/src/main/java/com/leafpic/app/SplashScreen.java
@@ -3,10 +3,12 @@ package com.leafpic.app;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,11 +45,12 @@ public class SplashScreen extends ThemedActivity {
         logo.setTextColor(getInvertedBackgroundColor());
 
 
-        setNavBarColor();
-        setStatusBarColor();
-        //getWindow().setNavigationBarColor(getInvertedBackgroundColor());
-        //getWindow().setStatusBarColor(getInvertedBackgroundColor());
-
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
     @Override
