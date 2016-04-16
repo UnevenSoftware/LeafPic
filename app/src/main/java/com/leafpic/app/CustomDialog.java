aa30c13029f1736208c855754aaaf45909889f65
Gilbert Ndresaj <jibo95@gmail.com>
2016-04-16 19:33:44 +0100

    DONY fai la CustomDialog Class se no Ti ammazzo

--- app/src/main/java/com/leafpic/app/CustomDialog.java
+++ app/src/main/java/com/leafpic/app/CustomDialog.java
@@ -11,4 +11,9 @@ public class CustomDialog extends ThemedActivity{
     // PER LE DIALOG OCN LSOLO TESTO TIPO ELIMINA ECC USA TEXT DIALOG
     // Mi rifuto solo per come l'hai inizata mi fai una classe dialog che eredita
     // la classe activity bello RIPIGLIATI!!!!!
+    // JIBO: BELLO NON FARE TANTO IL FIGO CHE ALLA FINE LO SAI CHE TI TOCCHERA
+    // PRIMA LA FAI MEGLIO è ALMENO TI TOGLI IL PROBLEMA...E POI RITTOLINE
+    // I R R E M O V I B I L E <------ NON PUOI SOTTRARTI AL TUO LAVORO
+    //SE NON LA FAI MI VEDO COSTRETTO A LICENZIARTI PERCIO FORSE è MEGLIO CHE LA FAI
+    // AH UN ALTRA COSA...FIXXA I VIDEO...
 }
