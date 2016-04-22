0cc607289607c7b7c03d4b59fffb8b72f467b156
Gilbert Ndresaj <jibo95@gmail.com>
2016-04-22 21:38:23 +0100

    White border to bitmap shortcut

--- app/src/main/java/com/horaapps/leafpic/Base/HandlingAlbums.java
+++ app/src/main/java/com/horaapps/leafpic/Base/HandlingAlbums.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.media.MediaScannerConnection;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -270,8 +272,7 @@ public class HandlingAlbums implements Parcelable {
             BitmapFactory.Options bmOptions = new BitmapFactory.Options();
             Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(),bmOptions);
             Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 128, 128, true);
-
-            addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, scaledBitmap);
+            addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, addWhiteBorder(scaledBitmap, 10));
             /*
             addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
                     Intent.ShortcutIconResource.fromContext(appCtx,
@@ -280,6 +281,12 @@ public class HandlingAlbums implements Parcelable {
             addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
             appCtx.sendBroadcast(addIntent);
         }
-
+    }
+    private Bitmap addWhiteBorder(Bitmap bmp, int borderSize) {
+        Bitmap bmpWithBorder = Bitmap.createBitmap(bmp.getWidth() + borderSize * 2, bmp.getHeight() + borderSize * 2, bmp.getConfig());
+        Canvas canvas = new Canvas(bmpWithBorder);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(bmp, borderSize, borderSize, null);
+        return bmpWithBorder;
     }
 }
