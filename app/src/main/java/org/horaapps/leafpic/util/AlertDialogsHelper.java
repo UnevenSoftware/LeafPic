64860bcfa2b866397d6e7655fe6e280e9c82f575
Gilbert Ndresaj <jibo95@gmail.com>
2016-12-28 18:39:07 +0000

    Changelog

--- app/src/main/java/org/horaapps/leafpic/util/AlertDialogsHelper.java
+++ app/src/main/java/org/horaapps/leafpic/util/AlertDialogsHelper.java
@@ -7,7 +7,7 @@ import android.net.Uri;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
-import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +31,8 @@ import org.horaapps.leafpic.model.base.MediaDetailsMap;
 
 import java.lang.reflect.Field;
 import java.util.Locale;
+
+import in.uncod.android.bypass.Bypass;
 
 /**
  * Created by dnld on 19/05/16.
@@ -199,15 +201,74 @@ public class AlertDialogsHelper {
 
         dialogTitle.setText(StringUtils.html(String.format(Locale.ENGLISH,"%s <font color='%d'>%s</font>", activity.getString(R.string.changelog), activity.getAccentColor(), BuildConfig.VERSION_NAME )));
 
-        Spanned changelogText = StringUtils.html("<b>#Fixed</b><br/>\n" +
-                "        &#8226; Fixed crash on startup and some random crash<br/>\n" +
-                "        &#8226; FIied crash opening video (Nougat)<br/>\n" +
-                "        &#8226; Fixed zoom out issue with SubScaling ImageView enabled<br/>\n" +
-                "        <b>#Update</b><br/>\n" +
-                "        &#8226; Updated translations<br/>\n" +
-                "        &#8226; General improvements<br/>");
+        Bypass bypass = new Bypass();
+        String markdownString = "An h1 header\n" +
+                "============\n" +
+                "\n" +
+                "Paragraphs are separated by a blank line.\n" +
+                "\n" +
+                "2nd paragraph. *Italic*, **bold**, and `monospace`. Itemized lists\n" +
+                "look like:\n" +
+                "\n" +
+                "  * this one\n" +
+                "  * that one\n" +
+                "  * the other one\n" +
+                "\n" +
+                "Note that --- not considering the asterisk --- the actual text\n" +
+                "content starts at 4-columns in.\n" +
+                "\n" +
+                "> Block quotes are\n" +
+                "> written like so.\n" +
+                ">\n" +
+                "> They can span multiple paragraphs,\n" +
+                "> if you like.\n" +
+                "\n" +
+                "Use 3 dashes for an em-dash. Use 2 dashes for ranges (ex., \"it's all\n" +
+                "in chapters 12--14\"). Three dots ... will be converted to an ellipsis.\n" +
+                "Unicode is supported. ☺\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "An h2 header\n" +
+                "------------\n" +
+                "\n" +
+                "Here's a numbered list:\n" +
+                "\n" +
+                " 1. first item\n" +
+                " 2. second item\n" +
+                " 3. third item\n" +
+                "\n" +
+                "Note again how the actual text starts at 4 columns in (4 characters\n" +
+                "from the left side). Here's a code sample:\n" +
+                "\n" +
+                "    # Let me re-iterate ...\n" +
+                "    for i in 1 .. 10 { do-something(i) }\n" +
+                "\n" +
+                "As you probably guessed, indented 4 spaces. By the way, instead of\n" +
+                "indenting the block, you can use delimited blocks, if you like:\n" +
+                "\n" +
+                "~~~\n" +
+                "define foobar() {\n" +
+                "    print \"Welcome to flavor country!\";\n" +
+                "}\n" +
+                "~~~\n" +
+                "\n" +
+                "(which makes copying & pasting easier). You can optionally mark the\n" +
+                "delimited block for Pandoc to syntax highlight it:\n" +
+                "\n" +
+                "~~~python\n" +
+                "import time\n" +
+                "# Quick, count to ten!\n" +
+                "for i in range(10):\n" +
+                "    # (but not *too* quick)\n" +
+                "    time.sleep(0.5)\n" +
+                "    print i\n" +
+                "~~~";
+        CharSequence string = bypass.markdownToSpannable(markdownString);
+        dialogMessage.setText(string);
+        dialogMessage.setMovementMethod(LinkMovementMethod.getInstance());
         dialogMessage.setTextColor(activity.getTextColor());
-        dialogMessage.setText(changelogText);
+
         changelogDialogBuilder.setView(dialogLayout);
         return changelogDialogBuilder.create();
     }
