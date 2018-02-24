d9a46a887899e218f0e267a9b6e5a94d576400af
Andy Caruso <macaruso@gmail.com>
2018-02-24 14:07:56 -0800

    Clean up Disable Animation behavior
    - Use show/hide on the toolbar in SingleMediaActivity instead of
      animating
    - Add an icon for the Disable Animation setting

--- app/src/main/java/org/horaapps/leafpic/activities/SingleMediaActivity.java
+++ app/src/main/java/org/horaapps/leafpic/activities/SingleMediaActivity.java
@@ -773,7 +773,7 @@ public class SingleMediaActivity extends SharedMediaActivity {
                     toolbar.animate().translationY(-toolbar.getHeight()).setInterpolator(new AccelerateInterpolator())
                             .setDuration(200).start();
                 } else {
-                    toolbar.animate().alpha(0.0f).setDuration(200).start();
+                    getSupportActionBar().hide();
                 }
 
                 getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
@@ -813,7 +813,7 @@ public class SingleMediaActivity extends SharedMediaActivity {
                     toolbar.animate().translationY(Measure.getStatusBarHeight(getResources())).setInterpolator(new DecelerateInterpolator())
                             .setDuration(240).start();
                 } else {
-                    toolbar.animate().alpha(1.0f).setDuration(240).start();
+                    getSupportActionBar().show();
                 }
                 getWindow().getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
