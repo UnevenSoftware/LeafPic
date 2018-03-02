fa75ce704389374f2028b72f5925068c76fb75e9
Calvin Noronha <calvin.nrnha@gmail.com>
2018-03-02 20:32:39 +0530

    Major About screen rework, added Calvin to developers
    - Added custom views for AboutDeveloper, AboutLink
    - Major rework in AboutActivity & activity_about
      - About ~500 lines shortened in activity_about.xml
      - Removed a lot of unnecessary ViewGroup nesting
      - Refactoring and cleanup in Activity
    - Renamed ApplicationData to ApplicationUtils.
    - Added Calvin to the list of developers :)

--- app/src/main/java/org/horaapps/leafpic/util/ServerConstants.java
+++ app/src/main/java/org/horaapps/leafpic/util/ServerConstants.java
@@ -8,6 +8,7 @@ public class ServerConstants {
     // GitHub URLs
     public static final String GITHUB_LEAFPIC = "https://github.com/HoraApps/LeafPic";
     public static final String GITHUB_DONALD = "https://github.com/DNLDsht";
+    public static final String GITHUB_CALVIN = "https://github.com/CalvinNor";
     public static final String GITHUB_GILBERT = "https://github.com/Mow3l";
 
     // LeafPic URLs
@@ -19,8 +20,10 @@ public class ServerConstants {
     // Google+ URLs
     public static final String GOOGLE_ABOUT_DONALD = "https://plus.google.com/103359244653769120543/about";
     public static final String GOOGLE_ABOUT_GILBERT = "https://plus.google.com/118430643662868782426/about";
+    public static final String GOOGLE_ABOUT_CALVIN = "https://plus.google.com/+CalvinNoronha2394";
 
     // Email IDs
     public static final String MAIL_DONALD = "dnld.sht@gmail.com";
     public static final String MAIL_GILBERT = "jibo95@gmail.com";
+    public static final String MAIL_CALVIN = "calvin.nrnha@gmail.com";
 }
