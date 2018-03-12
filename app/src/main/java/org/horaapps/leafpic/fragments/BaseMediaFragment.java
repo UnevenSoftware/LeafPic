d571adc38c6c7861000aff83f29421e10472fd23
Calvin Noronha <calvin.nrnha@gmail.com>
2018-03-12 21:47:14 +0530

    Issue: - When the user tries to rotate a GIF, the app crashes - When the user taps on Media to hide / show SystemUI,   the app crashes
    Root Cause:
    - Rotating Gifs is not a supported feature
    - When this action is tried, the code fetches the current fragment
      and casts it into an ImageFragment, which it's not
    - We call toggleSystemUI on the Activity, which can be null.
    
    Fixes:
    - Added a state boolean to maintain if we are showing the Image
      Menu (rotate, use as) or not.
    - We now only invalidate the menu on ViewPager swipe if the new
      media needs a different Menu (image - video - gif)
    - Added BaseMediaFragment to hold boilerplate code of getting the
      Media, and toggling System UI.
    - Removed all Activity dependencies in Media Fragments.
    - Minor cleanup and fixes.

---
+++ app/src/main/java/org/horaapps/leafpic/fragments/BaseMediaFragment.java
@@ -0,0 +1,73 @@
+package org.horaapps.leafpic.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+import org.horaapps.leafpic.data.Media;
+import org.horaapps.liz.ThemeHelper;
+import org.horaapps.liz.ThemedFragment;
+
+/**
+ * A Base Fragment for showing Media.
+ */
+public abstract class BaseMediaFragment extends ThemedFragment {
+
+    private static final String ARGS_MEDIA = "args_media";
+
+    protected Media media;
+    private MediaTapListener mediaTapListener;
+
+    @NonNull
+    protected static <T extends BaseMediaFragment> T newInstance(@NonNull T mediaFragment,
+                                                                 @NonNull Media media) {
+
+        Bundle args = new Bundle();
+        args.putParcelable(ARGS_MEDIA, media);
+        mediaFragment.setArguments(args);
+        return mediaFragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof MediaTapListener) mediaTapListener = (MediaTapListener) context;
+    }
+
+    private void fetchArgs() {
+        Bundle args = getArguments();
+        if (args == null) throw new RuntimeException("Must pass arguments to Media Fragments!");
+        media = getArguments().getParcelable(ARGS_MEDIA);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fetchArgs();
+    }
+
+    @Override
+    public void refreshTheme(ThemeHelper themeHelper) {
+        // Default implementation
+    }
+
+    protected void setTapListener(@NonNull View view) {
+        view.setOnClickListener(v -> onTapped());
+    }
+
+    private void onTapped() {
+        mediaTapListener.onViewTapped();
+    }
+
+    /**
+     * Interface for listeners to react on Media Clicks.
+     */
+    public interface MediaTapListener {
+
+        /**
+         * Called when user taps on the Media view.
+         */
+        void onViewTapped();
+    }
+}
--- app/src/main/java/org/horaapps/leafpic/fragments/VideoFragment.java
+++ app/src/main/java/org/horaapps/leafpic/fragments/VideoFragment.java
@@ -3,6 +3,8 @@ package org.horaapps.leafpic.fragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,72 +14,59 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 
-import org.horaapps.leafpic.activities.SingleMediaActivity;
+import org.horaapps.leafpic.R;
 import org.horaapps.leafpic.data.Media;
 import org.horaapps.leafpic.data.StorageHelper;
 import org.horaapps.liz.ThemeHelper;
-import org.horaapps.liz.ThemedFragment;
 import org.horaapps.liz.ui.ThemedIcon;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
- * Created by dnld on 18/02/16.
+ * A Media Fragment for showing a Video Preview.
  */
+public class VideoFragment extends BaseMediaFragment {
 
-public class VideoFragment extends ThemedFragment {
+    @BindView(R.id.media_view) ImageView previewView;
+    @BindView(R.id.video_play_icon) ThemedIcon playVideoIcon;
 
-    ThemedIcon videoInd;
+    @NonNull
+    public static VideoFragment newInstance(@NonNull Media media) {
+        return BaseMediaFragment.newInstance(new VideoFragment(), media);
+    }
 
-    private Media video;
-	
-	public static VideoFragment newInstance(Media media) {
-		VideoFragment videoFragment = new VideoFragment();
-		
-		Bundle args = new Bundle();
-		args.putParcelable("video", media);
-		videoFragment.setArguments(args);
-		
-		return videoFragment;
-	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		video = getArguments().getParcelable("video");
-	}
-	
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	                         Bundle savedInstanceState) {
-		
-		
-		View view =
-				inflater.inflate(org.horaapps.leafpic.R.layout.fragment_video, container, false);
-		
-		ImageView picture = view.findViewById(org.horaapps.leafpic.R.id.media_view);
-		videoInd = view.findViewById(org.horaapps.leafpic.R.id.icon);
-		videoInd.setOnClickListener(v -> {
-			Uri uri = StorageHelper.getUriForFile(getContext(), video.getFile());
-			Intent intent = new Intent(Intent.ACTION_VIEW).setDataAndType(uri, video.getMimeType());
-			intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-			startActivity(intent);
-		});
-		
-		
-		RequestOptions options =
-				new RequestOptions().signature(video.getSignature()).centerCrop()
-						.diskCacheStrategy(
-						DiskCacheStrategy.AUTOMATIC);
-		
-		
-		Glide.with(getContext()).load(video.getUri()).apply(options).into(picture);
-		
-		picture.setOnClickListener(v -> ((SingleMediaActivity) getActivity()).toggleSystemUI());
-		return view;
-	}
-	
-	@Override
-	public void refreshTheme(ThemeHelper themeHelper) {
-		videoInd.refreshTheme(themeHelper);
-	}
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_video, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        playVideoIcon.setOnClickListener(v -> {
+            Uri uri = StorageHelper.getUriForFile(getContext(), media.getFile());
+            Intent intent = new Intent(Intent.ACTION_VIEW).setDataAndType(uri, media.getMimeType());
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(intent);
+        });
+
+        // TODO: See where we can move this. Seems like boilerplate code that belongs in
+        // a utility class or Builder of some sort.
+        RequestOptions options =
+                new RequestOptions().signature(media.getSignature()).centerCrop()
+                        .diskCacheStrategy(
+                                DiskCacheStrategy.AUTOMATIC);
+
+        Glide.with(getContext()).load(media.getUri()).apply(options).into(previewView);
+        setTapListener(previewView);
+    }
+
+    @Override
+    public void refreshTheme(ThemeHelper themeHelper) {
+        playVideoIcon.refreshTheme(themeHelper);
+    }
 }
