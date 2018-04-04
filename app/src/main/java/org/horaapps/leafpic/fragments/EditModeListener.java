efddfc2232984fd43420ed63f75e7dade1533bae
Calvin Noronha <calvin.nrnha@gmail.com>
2018-04-04 22:19:34 +0530

    Fixed a selection count regression
    Issue:
    - When items are long pressed to enter selection mode and
      then select multiple, the count in Toolbar is not updated.
    
    Root cause:
    - Due to PR #527, we did not notify the Edit Mode listener
      more than once, only when Edit Mode is entered or exited.
    - Changing the toolbar count was a feature of Edit Mode listener.
    
    Fixes:
    - Added method onSelectionCountChanged to ActionsListener for
      notifying fragments about selection counts.
    - Added method onItemsSelected to EditModeListener to notify
      the Edit Mode listener about item selections.
    - Updated the Toolbar title when items are selected.
    - Changed title style from "1/10" to "1 of 10" for readability.

--- app/src/main/java/org/horaapps/leafpic/fragments/EditModeListener.java
+++ app/src/main/java/org/horaapps/leafpic/fragments/EditModeListener.java
@@ -10,4 +10,12 @@ import javax.annotation.Nullable;
 
 public interface EditModeListener {
     void changedEditMode(boolean editMode, int selected, int total, @Nullable View.OnClickListener listener, @Nullable String title);
+
+    /**
+     * Propagate the selected item count to listeners.
+     *
+     * @param count The number of items selected.
+     * @param total The total number of items.
+     */
+    void onItemsSelected(int count, int total);
 }
